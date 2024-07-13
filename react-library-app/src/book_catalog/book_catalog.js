35bbfbeb994ea3fe647f90a5e4738cf3e90d5138
AbdelRahman-Alwabarneh <abdelRahmanalwabarneh@gmail.com>
2024-07-14 00:43:27 +0300

    Add Update message

--- react-library-app/src/book_catalog/book_catalog.js
+++ react-library-app/src/book_catalog/book_catalog.js
@@ -19,27 +19,99 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-async function getBooks() {
-  try {
-    const response = await axios.get(
-      "https://library-app-79fea-default-rtdb.firebaseio.com/books.json"
-    );
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching data: ", error);
-  }
-}
-
 function BookCatalog() {
+  async function getBooks() {
+    try {
+      const response = await axios.get(
+        "https://library-app-79fea-default-rtdb.firebaseio.com/books.json"
+      );
+      return response.data ? Object.values(response.data) : [];
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  }
+  const [bookData, setBookData] = useState({});
+
+  async function PostBools() {
+    try {
+      await axios.post(
+        "https://library-app-79fea-default-rtdb.firebaseio.com/books.json",
+        {
+          id: bookData.id,
+          title: bookData.title,
+          author: bookData.author,
+          isbn: bookData.isbn,
+        }
+      );
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  }
+
+  async function UpdateBooks(book) {
+    try {
+      await axios.put(
+        `https://library-app-79fea-default-rtdb.firebaseio.com/books/${book.id}.json`,
+        {
+          id: book.id,
+          title: bookData.title,
+          author: bookData.author,
+          isbn: bookData.isbn,
+        }
+      );
+    } catch (error) {
+      console.error("Error updating book: ", error);
+    }
+  }
+
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    getBooks().then((dataBooka) => setBooks(dataBooka));
+    getBooks().then((dataBooks) => setBooks(dataBooks));
   }, []);
 
   return (
     <>
       <h1>Book Catalog</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          PostBools();
+        }}
+      >
+        <div className="All_input_books">
+          <div className="input_books">
+            <label>Title</label>
+            <input
+              type="text"
+              onChange={(e) =>
+                setBookData({ ...bookData, title: e.target.value })
+              }
+            />
+            <label>Author</label>
+            <input
+              type="text"
+              onChange={(e) =>
+                setBookData({ ...bookData, author: e.target.value })
+              }
+            />
+            <label>Isbn</label>
+            <input
+              type="text"
+              onChange={(e) =>
+                setBookData({ ...bookData, isbn: e.target.value })
+              }
+            />
+            <label>Id</label>
+            <input
+              type="text"
+              onChange={(e) => setBookData({ ...bookData, id: e.target.value })}
+            />
+
+            <input className="submit_button" type="submit" />
+          </div>
+        </div>
+      </form>
       <div className="AllCard">
         {books.map((book) => (
           <div className="Card">
@@ -50,6 +122,13 @@ function BookCatalog() {
             <h5>{book.isbn}</h5>
             <br />
             <h6>{book.id}</h6>
+            <div className="button_Card">
+              <button className="button_Edit" onClick={() => UpdateBooks(book)}>
+                Edit
+              </button>
+
+              <button className="button_Delete">Delete</button>
+            </div>
           </div>
         ))}
       </div>
